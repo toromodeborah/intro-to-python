836f4fe6aeb3e7b929b037e1835c44819f9d2218
toromodeborah <toromodeborah@gmail.com>
2026-08-20 20:03:17 +0300

    Add files via upload

---
+++ intro to python/main.py
@@ -0,0 +1,11 @@
+#printing a greeting message
+print("Hello, World!")
+
+#printing my name
+print("My name is Debra")
+
+#Printing a fun fact about me
+print("I love playing violin")
+
+#Performing and printing a simple calculation
+print("The result of 5 + 3 is:", 5 + 3)
